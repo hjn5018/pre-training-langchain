89fd33ae95b8b2e991bc12587d1e45dd528d9191
Your Name <you@example.com>
2025-07-02 10:25:58 +0900

    LangChain Test1

---
+++ main.py
@@ -0,0 +1,11 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI()
+result=llm.invoke("한국의 대표적인 관광지 3군데를 추천해주세요.")
+print(result)
