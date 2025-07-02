8cc4029d9b2ea059934f48abd3a123c06913d244
Your Name <you@example.com>
2025-07-02 10:47:45 +0900

    using module OpenAI

--- chain1.py
+++ chain1.py
@@ -1,10 +1,10 @@
-from langchain_openai import OpenAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+from langchain_openai import OpenAI
 
 llm = OpenAI()
 result=llm.invoke("한국의 대표적인 관광지 3군데를 추천해주세요.")
